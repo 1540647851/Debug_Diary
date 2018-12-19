09487c25c954149b98dd3421b750bfdede19205d
1540647851 <1540647851@qq.com>
2018-12-19 20:01:31 +0900

    Update 18.12.19.py

--- 18.12.19.py
+++ 18.12.19.py
@@ -1,1 +1,12 @@
-1. len
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 18 23:13:54 2018
+
+@author: Wang
+"""
+
+from llist import dllist
+a=dllist("123456")
+a.
+print(a)
