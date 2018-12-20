fe5ecd733664f436b5316232d8b65632ad2138b1
1540647851 <1540647851@qq.com>
2018-12-20 21:29:25 +0900

    Update and rename 18.12.19.py to 18.12.20.py

---
+++ 18.12.20.py
@@ -0,0 +1,10 @@
+1. A way to convert list to string
+    str="".join(list)
+    
+2. Char <-> ascii code
+    ascii=ord(char)
+    char=chr(ascii)
+ 
+4. Alphabet string's Capitolization
+    cap=str.upper()
+    
